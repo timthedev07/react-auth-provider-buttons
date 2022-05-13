37d658e5085ecb78f4174ec43d5b2a2e8f4e9f90
Tim <harvard90873@gmail.com>
2022-05-13 19:55:30 +0200

    fix follup

--- rollup.config.js
+++ rollup.config.js
@@ -10,7 +10,7 @@ const packageJson = require("./package.json");
 
 export default [
   {
-    input: "src/index.tsx",
+    input: "src/index.ts",
     output: [
       {
         file: packageJson.main,
@@ -29,7 +29,7 @@ export default [
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
       terser(),
-      postcss()
+      postcss(),
     ],
   },
   {
