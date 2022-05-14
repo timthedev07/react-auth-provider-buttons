abaa35dce99fcdf209a139802320943c4295bfa9
Tim <harvard90873@gmail.com>
2022-05-14 12:29:33 +0200

    fix

--- src/components/OAuthButton.tsx
+++ src/components/OAuthButton.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import { DiscordIcon } from "../icons/DiscordIcon";
 import { GoogleIcon } from "../icons/GoogleIcon";
 import { MicrosoftIcon } from "../icons/MicrosoftIcon";
@@ -8,12 +8,16 @@ import "../index.css";
 import { GithubIcon } from "../icons/GithubIcon";
 import { SpotifyIcon } from "../icons/SpotifyIcon";
 
-export interface OAuthButtonProps {
+export type OAuthButtonProps = {
   provider: ProviderType;
-  href: string | null | undefined;
+  href?: string | null | undefined;
   style?: React.CSSProperties;
   isDarkTheme?: boolean;
-}
+  className?: string;
+} & React.DetailedHTMLProps<
+  React.ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+>;
 
 const ICON_MAP = {
   discord: DiscordIcon,
@@ -26,28 +30,32 @@ const ICON_MAP = {
 /**
  * Returns a single <button> element, no links
  */
-export const OAuthButton: React.FC<OAuthButtonProps> = ({
-  provider,
-  href,
-  style,
-  isDarkTheme = false,
-}) => {
-  const Icon = ICON_MAP[provider];
+export const OAuthButton = forwardRef<HTMLButtonElement, OAuthButtonProps>(
+  ({ provider, href, style, isDarkTheme = false, onClick, ...props }, ref) => {
+    const Icon = ICON_MAP[provider];
 
-  return !href ? (
-    <button>No href provided</button>
-  ) : (
-    <button
-      onClick={() => {
-        window.location.href = href;
-      }}
-      className={`oauth-button oauth-button-${provider} ${
-        isDarkTheme ? "oauth-button-dark" : ""
-      }`}
-      style={style}
-    >
-      {capitalize(provider)}
-      <Icon className="oauth-button__icon" />
-    </button>
-  );
-};
+    return (
+      <button
+        ref={ref}
+        {...props}
+        onClick={(e) => {
+          if (onClick) {
+            if (href) {
+              window.location.href = href;
+            }
+            onClick(e);
+          } else if (href) {
+            window.location.href = href;
+          }
+        }}
+        className={`oauth-button oauth-button-${provider} ${
+          isDarkTheme ? "oauth-button-dark" : ""
+        }`}
+        style={style}
+      >
+        {capitalize(provider)}
+        <Icon className="oauth-button__icon" />
+      </button>
+    );
+  }
+);
